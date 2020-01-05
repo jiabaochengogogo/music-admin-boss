ae3c654753756d890f20af593b0eef5f0bc75b94
chenjiabaogogogo <1053685823@qq.com>
2020-01-05 22:51:21 +0800

    项目初始始化

---
+++ assets/config/http.js
@@ -0,0 +1,7 @@
+const httpUrl = 'http://127.0.0.1:7005'
+const loginUrl = httpUrl + '/test/login/do'
+
+
+module.exports = {
+    loginUrl
+}
---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+function getCookie(cookieName){
+    var cookieStr = unescape(document.cookie)
+    var arr = cookieStr.split("; ")
+    var cookieValue = ""
+    for(var i=0; i<arr.length; i++){
+      var temp = arr[i].split("=");
+      if(temp[0] == cookieName){
+        cookieValue = temp[1]
+        break
+      }
+    }
+    return cookieValue
+  }
